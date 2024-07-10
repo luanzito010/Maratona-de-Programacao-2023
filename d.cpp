d41769e05448e9d4bdf31f4f933181bb4c3f1a0d
luanzito010 <140329902+luanzito010@users.noreply.github.com>
2024-07-10 13:25:30 -0400

    Update d.cpp

--- d.cpp
+++ d.cpp
@@ -1,65 +1,80 @@
 #include <bits/stdc++.h>
+#define int long long 
+#define pb push_back
+#define fr first
+#define sc second
+#define bg begin()
+#define ed end()
+#define all(x) x.begin(),x.end()
+#define sz(x) (int) x.size()
+#define debug(x) cout << x << endl;
 
 using namespace std;
-using ll = long long int;
-const ll maxn=3e2+10,inf=1e18+10;
+using pii = pair<int,int>;
+using tii = tuple<int,int,int>;
 
-int a,b,c,i,j,k,n,m,dist,u,mark[maxn];
-ll d[maxn],mat[maxn][maxn][2];
-vector<pair<int,int>>pares,g[maxn];
+const int maxn = 3e2 + 10,inf = 1e9;
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-    cin>>n>>m;
+int n,m;
+vector<pii> edg;
+vector<vector<int>> dist(maxn,vector<int>(maxn,inf)),ex[maxn];
+
+void fw(int l,int r,vector<vector<int>> &dist){
     
-    for(i=1;i<=n;i++)
-        for(j=1;j<=n;j++){
-            if(i==j) mat[i][j][0]=mat[i][j][1]=0;
-            else mat[i][j][0]=mat[i][j][1]=inf;
-        }
+    for(int k = l;k <= r;k++)
+        for(int i = 1;i <= n;i++)
+            for(int j = 1;j <= n;j++)
+                dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j]);
     
-    for(i=1;i<=m;i++){
-        cin>>a>>b>>c;
-        mat[a][b][0]=mat[b][a][0]=mat[a][b][1]=mat[b][a][1]=c;
-        g[a].push_back({b,c});
-        g[b].push_back({a,c});
-        pares.push_back({a,b});
+}
+
+void daq(int l,int r,vector<vector<int>> &dist){
+   
+    if(l == r){ 
+        ex[l] = dist;
+        return;
     }
     
-    for(k=1;k<=n;k++)
-        for(i=1;i<=n;i++)
-            for(j=1;j<=n;j++)
-                mat[i][j][1]=min(mat[i][j][1],mat[i][k][1]+mat[k][j][1]);
-    
-    for(auto [a,b]:pares) {
-        if(mat[a][b][1]<mat[a][b][0]) cout<<mat[a][b][1]<<"\n";
-        else{
-        
-        for(int i=1;i<=n;i++)
-            d[i]=inf,mark[i]=0;
-        
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>f;
-        d[a]=0;
-        f.push({d[a],a});
-        while(!f.empty()){
-            dist=f.top().first,u=f.top().second;
-            f.pop();
-            if(mark[u]) continue;
-            mark[u]=1;
-            for(auto [v,w]:g[u]){
-                if(u==a&&v==b||u==b&&v==a) continue;
-                if(d[v]>dist+w){
-                    d[v]=dist+w;
-                    f.push({d[v],v});
-                }
-            }
-        }
-        cout<<(d[b]==inf?-1:d[b])<<"\n";
-    }
-    }
+    vector<vector<int>> aux = dist;    
+    int m = (l + r) >> 1;
 
+    fw(l,m,dist);
+    daq(m + 1,r,dist);
+    fw(m + 1,r,aux);
+    daq(l,m,aux);
+    
+}
+
+void solve(){
+    
+    cin >> n >> m;
+    
+    for(int i = 1;i <= m;i++){
+        int u,v,w;
+        cin >> u >> v >> w;
+        edg.pb({u,v});
+        dist[u][v] = dist[v][u] = w;
+    }
+    
+    daq(1,n,dist);
+    
+    for(auto [u,v] : edg){
+        int resp = inf;
+        for(int i = 1;i <= n;i++)
+            if(u != i && v != i)
+                resp = min(resp,ex[v][u][i] + ex[u][i][v]);
+        cout << (resp == inf ? -1 : resp) << "\n";
+    }
+    
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        solve();
     return 0;
 }
